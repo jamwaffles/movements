90a48d207314ecf5998259d638853f0f9245e761
James Waples <james@wapl.es>
2020-06-18 13:01:11 +0100

    Add spindle speed support

--- gcode-parser/src/token.rs
+++ gcode-parser/src/token.rs
@@ -62,6 +62,9 @@ pub enum TokenType {
     /// Spindle
     Spindle(Spindle),
 
+    /// Spindle speed (RPM)
+    SpindleSpeed(f32),
+
     /// Stopping
     Stopping(Stopping),
 
@@ -112,8 +115,11 @@ pub fn token(i: ParseInput) -> IResult<ParseInput, Token> {
         map(distance_mode, TokenType::DistanceMode),
         map(cutter_compensation, TokenType::CutterCompensation),
         map(tool_change, TokenType::ToolChange),
-        map(word('T'), |w| TokenType::Tool(w.value)),
         map(comment, TokenType::Comment),
+        map(word('T'), |w| TokenType::Tool(w.value)),
+        map(verify(word('S'), |w| w.value >= 0.0), |w| {
+            TokenType::SpindleSpeed(w.value)
+        }),
         map(
             verify(word::<f32, _>('F'), |w| w.value.is_sign_positive()),
             |w| TokenType::FeedRate(w.value),
@@ -124,7 +130,7 @@ pub fn token(i: ParseInput) -> IResult<ParseInput, Token> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::rem;
+    use crate::{rem, tok};
     use nom::error::ErrorKind;
     use nom::Err::Error;
 
@@ -157,6 +163,41 @@ mod tests {
         assert_eq!(
             token(ParseInput::new("N1 G40")),
             Err(Error((rem!("N1 G40", 0, 1), nom::error::ErrorKind::Verify)))
+        );
+    }
+
+    #[test]
+    fn spindle_speed() {
+        assert_eq!(
+            token(ParseInput::new("S0")),
+            Ok((
+                rem!("", 2),
+                tok!(TokenType::SpindleSpeed(0.0), offs = (0, 2)),
+            ))
+        );
+
+        assert_eq!(
+            token(ParseInput::new("S5000")),
+            Ok((
+                rem!("", 5),
+                tok!(TokenType::SpindleSpeed(5000.0), offs = (0, 5)),
+            ))
+        );
+
+        assert_eq!(
+            token(ParseInput::new("S340.6")),
+            Ok((
+                rem!("", 6),
+                tok!(TokenType::SpindleSpeed(340.6), offs = (0, 6)),
+            ))
+        );
+
+        assert_eq!(
+            token(ParseInput::new("S-200.0")),
+            Err(Error((
+                rem!("S-200.0", 0, 1),
+                nom::error::ErrorKind::Verify
+            )))
         );
     }
 }
