ac25da1c52e6118cb1f1b49bbb6742a262cfd2d8
James Waples <james@wapl.es>
2020-11-16 14:26:05 +0000

    Simpler block line parsing

--- streaming-gcode-parser/src/lib.rs
+++ streaming-gcode-parser/src/lib.rs
@@ -11,7 +11,7 @@ use block::Block;
 use nom::{
     character::streaming::line_ending,
     combinator::all_consuming,
-    multi::{many0, separated_list0},
+    multi::{many0, many1, separated_list0},
     sequence::terminated,
     IResult,
 };
@@ -26,23 +26,9 @@ pub struct Program {
 
 impl Program {
     pub fn parse_complete(i: &str) -> IResult<&str, Self> {
-        let mut i = i;
-        let mut blocks = Vec::new();
+        let (i, blocks) = separated_list0(many1(line_ending), Block::parse)(i)?;
 
-        loop {
-            match terminated(Block::parse, line_ending)(i) {
-                Err(nom::Err::Error(_)) => break,
-                Err(e) => return Err(e),
-                Ok((i1, o)) => {
-                    blocks.push(o);
-                    i = i1;
-                }
-            }
-
-            if i.is_empty() {
-                break;
-            }
-        }
+        // println!("{:#?}", blocks);
 
         // debug_assert!(i.is_empty(), "Remaining input: {}", i);
 
