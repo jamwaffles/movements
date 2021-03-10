4d5b95f8990c95a6ca4c8160150b351b1530d310
James Waples <james@wapl.es>
2021-03-10 20:16:19 +0000

    Support G2/G3 parsing

--- streaming-gcode-parser/src/modal_groups/motion.rs
+++ streaming-gcode-parser/src/modal_groups/motion.rs
@@ -7,6 +7,16 @@ use nom::{
     combinator::not, sequence::terminated, IResult,
 };
 
+/// Arc direction
+#[derive(Debug, PartialEq, Clone)]
+pub enum ArcDirection {
+    /// Clockwise
+    CW,
+
+    /// Counter-clockwise.
+    CCW,
+}
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum Motion {
     /// `G0`
@@ -14,18 +24,27 @@ pub enum Motion {
 
     /// `G1`
     Feed,
+
+    /// `G2`/`G3` CW/CCW arc
+    Arc(ArcDirection),
 }
 
 impl Motion {
     pub fn parse(i: Span) -> IResult<Span, Self> {
         let short_g0 = terminated(tag_no_case("G0"), not(digit1));
         let short_g1 = terminated(tag_no_case("G1"), not(digit1));
+        let short_g2 = terminated(tag_no_case("G2"), not(digit1));
+        let short_g3 = terminated(tag_no_case("G3"), not(digit1));
 
         alt((
             map(tag_no_case("G00"), |_| Motion::Rapid),
             map(tag_no_case("G01"), |_| Motion::Feed),
+            map(tag_no_case("G02"), |_| Motion::Arc(ArcDirection::CW)),
+            map(tag_no_case("G03"), |_| Motion::Arc(ArcDirection::CCW)),
             map(short_g0, |_| Motion::Rapid),
             map(short_g1, |_| Motion::Feed),
+            map(short_g2, |_| Motion::Arc(ArcDirection::CW)),
+            map(short_g3, |_| Motion::Arc(ArcDirection::CCW)),
         ))(i)
     }
 }
@@ -45,6 +64,10 @@ mod tests {
         assert_parse!(Motion::parse, "g00;", (";", Motion::Rapid));
         assert_parse!(Motion::parse, "G1;", (";", Motion::Feed));
         assert_parse!(Motion::parse, "G01;", (";", Motion::Feed));
+        assert_parse!(Motion::parse, "g2;", (";", Motion::Arc(ArcDirection::CW)));
+        assert_parse!(Motion::parse, "g02;", (";", Motion::Arc(ArcDirection::CW)));
+        assert_parse!(Motion::parse, "G3;", (";", Motion::Arc(ArcDirection::CCW)));
+        assert_parse!(Motion::parse, "G03;", (";", Motion::Arc(ArcDirection::CCW)));
 
         assert_eq!(
             Motion::parse("G04;".into()),
