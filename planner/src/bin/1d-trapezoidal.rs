7738b9af2707191d699f7bc3c14ffb3d0804488f
James Waples <james@wapl.es>
2021-08-14 12:23:13 +0100

    Precompute a bunch of stuff

--- planner/src/bin/1d-trapezoidal.rs
+++ planner/src/bin/1d-trapezoidal.rs
@@ -1,5 +1,7 @@
 // Favourite calculator: <https://www.calculatorsoup.com/calculators/physics/>
 
+use std::ops;
+
 #[derive(Debug, Copy, Clone)]
 pub struct Vertex {
     pub position: f32,
@@ -19,13 +21,19 @@ fn p_2(t: f32, initial_position: f32, initial_velocity: f32, acceleration: f32) 
     initial_position + (initial_velocity * t) + (0.5 * acceleration * t.powi(2))
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Clone)]
 pub struct Segment {
     delta_t1: f32,
     delta_t2: f32,
     delta_t3: f32,
 
-    time: f32,
+    t1: f32,
+    t2: f32,
+    t3: f32,
+
+    range_t1: ops::Range<f32>,
+    range_t2: ops::Range<f32>,
+    range_t3: ops::RangeInclusive<f32>,
 
     delta_x1: f32,
     delta_x2: f32,
@@ -57,8 +65,8 @@ impl Segment {
                 // Below velocity limit - accelerate
                 sign
             } else {
-                // Too fast for current limit - decelerate. This will result in a double-deceleration
-                // profile.
+                // Too fast for current limit - decelerate. This will result in a
+                // double-deceleration profile.
                 -sign
             };
 
@@ -83,8 +91,8 @@ impl Segment {
         let mut delta_t2 =
             (end.position - (start.position + delta_x1 + delta_x3)) / cruise_velocity;
 
-        // Not enough space/time to create a trapezoidal profile. We'll reduce the maximum velocity and
-        // recalculate everything to form a "wedge" shaped profile.
+        // Not enough space/time to create a trapezoidal profile. We'll reduce the maximum velocity
+        // and recalculate everything to form a "wedge" shaped profile.
         if delta_t2 < 0.0 {
             // New limit for cruise velocity
             cruise_velocity = f32::sqrt(
@@ -109,8 +117,19 @@ impl Segment {
         // Cruise displacement (will be 0 if a wedge shaped profile is formed)
         let delta_x2 = cruise_velocity * delta_t2;
 
+        // Time at end of first phase
+        let t1 = delta_t1;
+
+        // Time at end of cruise phase
+        let t2 = delta_t1 + delta_t2;
+
         // Total segment time
         let t3 = delta_t1 + delta_t2 + delta_t3;
+
+        let range_t1 = 0.0..t1;
+        let range_t2 = t1..t2;
+        // NOTE: End-inclusive to return correct value for t = t3
+        let range_t3 = t2..=t3;
 
         Self {
             delta_x1,
@@ -121,7 +140,13 @@ impl Segment {
             delta_t2,
             delta_t3,
 
-            time: t3,
+            t1,
+            t2,
+            t3,
+
+            range_t1,
+            range_t2,
+            range_t3,
 
             start,
             end,
@@ -133,12 +158,8 @@ impl Segment {
     }
 
     pub fn position(&self, t: f32) -> f32 {
-        let t1 = 0.0..self.delta_t1;
-        let t2 = self.delta_t1..self.delta_t1 + self.delta_t2;
-        let t3 = self.delta_t1 + self.delta_t2..self.delta_t1 + self.delta_t2 + self.delta_t2;
-
         // Acceleration phase t1 (or deceleration if we were originally moving too fast)
-        if t1.contains(&t) {
+        if self.range_t1.contains(&t) {
             p_2(
                 t,
                 self.start.position,
@@ -147,22 +168,22 @@ impl Segment {
             )
         }
         // Cruise phase t2
-        else if t2.contains(&t) {
+        else if self.range_t2.contains(&t) {
             // Position at end of t1
             let x1 = p_2(
-                self.delta_t1,
+                self.t1,
                 self.start.position,
                 self.start.velocity,
                 self.acceleration,
             );
 
-            p_2(t - self.delta_t1, x1, self.cruise_velocity, 0.0)
+            p_2(t - self.t1, x1, self.cruise_velocity, 0.0)
         }
         // Deceleration t3
-        else if t3.contains(&t) {
+        else if self.range_t3.contains(&t) {
             // Position at end of t1
             let x1 = p_2(
-                self.delta_t1,
+                self.t1,
                 self.start.position,
                 self.start.velocity,
                 self.acceleration,
@@ -171,59 +192,43 @@ impl Segment {
             // Position at end of cruise phase
             let x2 = x1 + (self.cruise_velocity * self.delta_t2);
 
-            let t2 = self.delta_t1 + self.delta_t2;
-
-            p_2(t - t2, x2, self.cruise_velocity, self.deceleration)
-        }
-        // The absolute last position of this segment
-        else {
-            self.end.position
+            p_2(t - self.t2, x2, self.cruise_velocity, self.deceleration)
+        } else {
+            unreachable!()
         }
     }
 
     pub fn velocity(&self, t: f32) -> f32 {
-        let t1 = 0.0..self.delta_t1;
-        let t2 = self.delta_t1..self.delta_t1 + self.delta_t2;
-        let t3 = self.delta_t1 + self.delta_t2..self.delta_t1 + self.delta_t2 + self.delta_t2;
-
         // Acceleration phase t1 (or deceleration if we were originally moving too fast)
-        if t1.contains(&t) {
+        if self.range_t1.contains(&t) {
             self.start.velocity + self.acceleration * t
         }
         // Cruise phase t2
-        else if t2.contains(&t) {
+        else if self.range_t2.contains(&t) {
             self.cruise_velocity
         }
         // Deceleration t3
-        else if t3.contains(&t) {
-            self.cruise_velocity + self.deceleration * (t - (self.delta_t1 + self.delta_t2))
-        }
-        // The absolute last position of this segment
-        else {
-            0.0
+        else if self.range_t3.contains(&t) {
+            self.cruise_velocity + self.deceleration * (t - self.t2)
+        } else {
+            unreachable!()
         }
     }
 
     pub fn acceleration(&self, t: f32) -> f32 {
-        let t1 = 0.0..self.delta_t1;
-        let t2 = self.delta_t1..self.delta_t1 + self.delta_t2;
-        let t3 = self.delta_t1 + self.delta_t2..self.delta_t1 + self.delta_t2 + self.delta_t2;
-
         // Acceleration phase t1 (or deceleration if we were originally moving too fast)
-        if t1.contains(&t) {
+        if self.range_t1.contains(&t) {
             self.acceleration
         }
         // Cruise phase t2
-        else if t2.contains(&t) {
+        else if self.range_t2.contains(&t) {
             0.0
         }
         // Deceleration t3
-        else if t3.contains(&t) {
+        else if self.range_t3.contains(&t) {
             self.deceleration
-        }
-        // The absolute last position of this segment
-        else {
-            self.deceleration
+        } else {
+            unreachable!()
         }
     }
 }
@@ -247,11 +252,13 @@ fn main() {
 
     let segment = Segment::new(start, end, &limits);
 
-    dbg!(segment);
+    dbg!(core::mem::size_of::<Segment>());
+
+    dbg!(&segment);
 
     let mut x = 0.0;
 
-    while x <= segment.time {
+    while x <= segment.t3 {
         let pos = segment.position(x);
         let vel = segment.velocity(x);
         let acc = segment.acceleration(x);
